504eb8c7dfa77d600b3715c4973654cf07ae0668
Diego Henrique Silva <diegosilvadhs@gmail.com>
2019-10-26 14:41:19 -0300

    Exemplo1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ExerciciosAngular';
+
+  nomes: string[] = ['joão', 'maria','josé','pedro','felipe','carlos']
+
 }
